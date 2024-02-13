225654965be294fd8ee9d737383c3416f3a6bea9
rainekomata <156048795+rainekomata@user.noreply.github.com.>
2024-02-12 21:44:11 -0700

    281 Lab 1

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth.vhd
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
---| CREATED       : 12/12/2019 Last Modified 06/24/2020
+--| AUTHOR(S)     : Raine Komata
+--| CREATED       : 02/12/2024 
 --| DESCRIPTION   :  This file implements the thirtyOneDaMonth lab.  Using a 4
 --| switch input, the circuit will light up whenever the 4 switches represent a
 --| month with 31 days
@@ -56,10 +56,12 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
+
 						-- output
+	o_Y : out std_logic
   );
 end thirtyOneDayMonth;
 
@@ -67,13 +69,26 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 	-- include components declarations and signals
 	
 	--signals internal to the architecture are declared and initialized such as w_sel
+    signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
   
 begin
+        
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
 	--finish assigning signals
-	
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+    with w_sel select 
+        o_Y <= (NOT i_D) when "100",
+               (NOT i_D) when "101",
+               (NOT i_D) when "110",
+               (NOT i_D) when "111",
+               i_D when "000",
+               i_D when "001",
+               i_D when "010",
+               i_D when "011",
+               '0' when others;
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
--- src/hdl/thirtyOneDayMonth_tb.vhd
+++ src/hdl/thirtyOneDayMonth_tb.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth_tb.vhd (TEST BENCH)
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
---| CREATED       : 12/12/2019 Last Modified 06/24/2020
+--| AUTHOR(S)     : Raine Komata
+--| CREATED       : 02/1/2024
 --| DESCRIPTION   : This file tests to ensure thirtyOneDayMonthMux works properly
 --|
 --|
@@ -93,8 +93,29 @@ begin
 		w_sw <= x"0"; wait for 10 ns;
             assert w_Y = '0' report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jan" severity failure;   
-
+            assert w_Y = '1' report "error on Jan" severity failure;
+        w_sw <= x"2"; wait for 10 ns;
+            assert w_Y = '0' report "error on Feb" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_Y = '1' report "error on Mar" severity failure;  
+        w_sw <= x"4"; wait for 10 ns;
+            assert w_Y = '0' report "error on Apr" severity failure; 
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_Y = '1' report "error on May" severity failure;
+        w_sw <= x"6"; wait for 10 ns;
+            assert w_Y = '0' report "error on Jun" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_Y = '1' report "error on Jul" severity failure;
+        w_sw <= x"8"; wait for 10 ns;
+            assert w_Y = '1' report "error on Aug" severity failure;
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_Y = '0' report "error on Sept" severity failure;
+        w_sw <= x"A"; wait for 10 ns;
+            assert w_Y = '1' report "error on Oct" severity failure;
+        w_sw <= x"B"; wait for 10 ns;
+            assert w_Y = '0' report "error on Nov" severity failure;
+        w_sw <= x"C"; wait for 10 ns;
+            assert w_Y = '1' report "error on Dec" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
